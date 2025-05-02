5801d6d2b8e78183d98fb4a77b8b7450fab7a174
Fernando <gilcorrealuis09@gmail.com>
2025-05-01 22:46:47 -0500

    🚀 feature/event-management: setup event-related database tables

---
+++ database/migrations/2025_05_02_032406_create_evento_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evento', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->foreignId('coordinador_id')
+                ->nullable()
+                ->constrained('usuario');
+            $table->dateTime('fecha_inicio');
+            $table->dateTime('fecha_fin');
+            $table->string('ubicacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evento');
+    }
+};
