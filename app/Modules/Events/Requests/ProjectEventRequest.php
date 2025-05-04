8f24586d1e6b6dff2c72e18372781874a2cc9337
Fernando <gilcorrealuis09@gmail.com>
2025-05-04 14:39:32 -0500

    🐛 fix request/ProjectEventRequest: remove custom validation messages for project event fields

--- app/Modules/Events/Requests/ProjectEventRequest.php
+++ app/Modules/Events/Requests/ProjectEventRequest.php
@@ -20,14 +20,4 @@ class ProjectEventRequest extends FormRequest
         ];
     }
 
-    public function messages()
-    {
-        return [
-            'proyecto_id.required' => 'El ID del proyecto es requerido',
-            'proyecto_id.integer' => 'El ID del proyecto debe ser un número entero',
-            'proyecto_id.min' => 'El ID del proyecto debe ser positivo',
-            'fecha_inscripcion.required' => 'La fecha de inscripción es requerida',
-            'fecha_inscripcion.date' => 'La fecha de inscripción debe ser válida'
-        ];
-    }
 }
